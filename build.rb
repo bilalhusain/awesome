40c1682a6088048b71b044e6331eb4e35b8605df
Bilal Husain <bilal@bilalhusain.com>
2011-08-21 10:42:27 +0000

    with category

--- build.rb
+++ build.rb
@@ -6,31 +6,63 @@ require './config'
 #require 'rubygems'
 #Gem.path.push GEMS_PATH
 
+require 'find'
 require 'haml'
 require 'bluecloth'
 
-def render(content, timestamp, destFile)
-	template = File.read(STATIC_DIR + '/views/layout.haml') # optimize, my foot
-	haml_engine = Haml::Engine.new(template)
-	txt = haml_engine.render(Object.new, {:content => content, :timestamp => timestamp})
+#for archive listing
+$posts = []
+$haml_engine = Haml::Engine.new(File.read("#{STATIC_DIR}/views/layout.haml"))
+
+def generateTimestamp(path)
+	f_ctime = File.stat(path).ctime.utc
+	f_ctime += 19800 # +05:30 for IST
+	f_ctime.strftime("last modified on %d %b %Y, %I:%M %p")
+end
+
+def renderPost(srcFile, destFile)
+	# read the contents
+	contents = File.read(srcFile)
+	html = BlueCloth::new(contents).to_html
+
+	# get the timestamp
+	timestamp = generateTimestamp(srcFile)
+
+	# render
+	locals = {:content => html, :timestamp => timestamp}
+	txt = $haml_engine.render(Object.new, locals)
 
 	# and write it to the destination
 	f = File.open(destFile, 'w')
 	f.write(txt)
 	f.close()
+
+	# add to list
+	$posts.push({:path => destFile, :timestamp => timestamp})
 end
 
-md_filenames = Dir.new(CONTENT_DIR).entries.select { |f| f =~ /\.md$/ }
+# generate the posts
+Find.find(CONTENT_DIR) do |path|
+	next if not(FileTest.file?(path) and (path =~ /\.md$/))
 
-md_filenames.each do |filename|
-	f = "#{CONTENT_DIR}/#{filename}"
-	contents = File.read(f)
-	html = BlueCloth::new(contents).to_html
-
-	publish_filename = /^(.*)\.md$/.match(filename)[1] + '.html'
-	f_ctime = File.stat(f).ctime.utc + 19800 # +05:30 for IST
-	timestamp = f_ctime.strftime("last modified on %d %b %Y, %I:%M %p")
-
-	render(html, timestamp, "#{PUBLISH_DIR}/#{publish_filename}")
+	filename = path.slice(CONTENT_DIR.length..-1)
+	srcFile = "#{CONTENT_DIR}/#{filename}"
+	destFile = "#{PUBLISH_DIR}/#{filename.gsub(/\.md$/, '.html')}"
+	renderPost(srcFile, destFile)
 end
+
+# generate the archive
+$posts.sort! { |x, y| y[:timestamp] <=> x[:timestamp] } # latest post first
+
+f = File.open("#{CONTENT_DIR}/browse.tmp", 'w')
+f.write("Archive\n---\n\n")
+$posts.each do |post|
+	url = post[:path].slice(PUBLISH_DIR.length..-1)
+	url.gsub!(/^\/+/, '') # remove preceding /
+	f.write("[#{url}](#{url}), at #{post[:timestamp]}\n")
+end
+f.close()
+renderPost(f.path, "#{PUBLISH_DIR}/browse.htm") # .htm file won't conflict
+
+# copy static files
 
